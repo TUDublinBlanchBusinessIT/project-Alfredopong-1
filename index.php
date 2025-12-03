80ee34e625bac3d8e48f9d6ba8e2e8485badb658
Alfred Ukpong <B00165631@mytudublin.ie>
2025-12-03 22:20:09 +0000

    Updated the index.php to use db_connect & added basic homepage links

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'db_connect.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Game Backlog</title>
+</head>
+<body>
+
+    <h1>Welcome to the Game Backlog App</h1>
+
+    <p>You are successfully connected to the database.</p>
+
+    <p><a href="add_game.php">Add a New Game</a></p>
+    <p><a href="view_games.php">View Games</a></p>
+
+</body>
+</html>
